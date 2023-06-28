7ff7c9990d98af261c4ce43f3ab01ed0be61ed7d
jagansai <jagan.sai@outlook.com>
2023-06-28 12:15:27 +0800

    initial repo

---
+++ learning/learning.py
@@ -0,0 +1,74 @@
+# functions
+
+# write function to return true if even else false
+def is_even(num ) :
+    if num % 2 == 0 :
+        return True
+    else :
+        return False
+
+def is_odd(num ) :
+    return is_even(num) == False
+
+
+# Let's start with basisc
+
+
+print( "Hello, world" )
+print("Hello, Neeraja")
+print("Hello, Jagan")
+name = input("Enter name: ")
+
+print(f"Hello, {name}")
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+print( f"Sum of {num1} + {num2} = {num1 + num2}")
+
+
+nums = [1,2,3,4,5, 6, 7, 8, 9, 10 ]
+names = ["sai", "jagan", "pantula"]
+
+print(f"Printing: {nums}")
+
+for num in nums:
+    print(f"{num}")
+
+print("Print numbers in reverse")
+
+for num in nums[::-1]:
+    print(f"{num}")
+
+print("Print every alt number in nums")
+
+for index in range(0, len(nums) -2, 4):
+    print(f"{index}")
+
+print(f"Only even numbers in the list of {nums=}")
+
+for num in nums:
+    if is_even(num) == True:
+        print(f"{num}")
+
+print(f"only odd numbers in the list of {nums=}")
+
+for num in nums:
+    if is_odd(num):
+        print(f"{num}")
+
+# in a list of numbers [1,2,3,4,5,6,7,8,9,10] count the number of evens and number of odds.
+# o/p: number of evens: ?
+#      number of odds: ?
+
+evens = 0
+odds = 0
+
+for num in nums:
+    if is_even(num):
+        evens += 1
+    else:
+        odds += 1
+
+print(f"Number of evens:{evens}")
+print(f"Number of odds:{odds}")
---
+++ learning/sample.py
@@ -0,0 +1,29 @@
+#find given num is even or odd
+num = int(input("enter num: "))
+
+if num % 2 == 0:
+ print (f"{num} is even")
+else:
+
+ print(f"{num} is odd")
+
+
+#check whether the given integer is a multiple of both 5 and 7
+
+if (num % 5 == 0) and (num % 7 == 0):
+  print (f"{num} is multiple of 5 and 7")
+else:
+  print (f"{num} not multiple of 5 and 7 ")
+
+#Python program to find the average of 10 numbers using while loop
+
+
+
+
+
+
+
+
+
+
+
