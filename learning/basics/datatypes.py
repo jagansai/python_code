8b1478d4525c1163760035e1e950a6da37dd44ca
Sai Jagannath <jagan.sai@outlook.com>
2024-03-31 11:16:24 +0800

    Added new code

---
+++ learning/basics/datatypes.py
@@ -0,0 +1,113 @@
+'''
+ - numbers
+    - integers : -1 ....0, 1 , 2, 3, ...
+    - decimals : -1.2 , 0, 1.1, 2.2
+    - fractions : 0.5, 0.6
+    
+ - text : "This is a text". "ABC" . This can be represented in python with double or single quotes
+ - boolean : True or False.
+'''
+
+
+''''
+A data is contained in a variable. The variable names cannot be seperated by a space.
+For e.g., 
+Below are valid variable names
+num
+nums
+num2
+num_of_evens
+
+Below are invalid variable names
+-num
+num of evens 
+'''
+
+
+'''
+In python, we don't need to specify the type of the data.
+For e.g., you don't need to specify if the data is number or text or boolean
+'''
+
+a = 10
+
+print( "Printing the data in the variable 'a' having number:" , a)
+
+a = "This is a text"
+
+print("Printing the data in the variable 'a' having text: ", a)
+
+a = True
+
+print("Printing the data in the variable 'a' having text: ", a)
+
+
+
+
+num = 10
+# increment num by 10.
+
+num = num + 10
+
+print(f"The value of num is {num}")
+
+
+# decrement num by 5
+
+num = num - 5
+print(f"The value of num is {num}")
+
+# multiply num by 5
+
+num = num * 5
+print(f"The value of num is {num}")
+
+# divide num by 5
+num = num / 5
+print(f"The value of num is {num}")
+
+
+# some operations on text.
+
+text = "this is a text"
+
+print(f"Printing the contents of text: {text}")
+
+text2 = text.upper()
+
+print(f"Printing the contents of text: {text2}")
+
+text2 = text.capitalize()
+
+print(f"Printing the contents of text: {text2}")
+
+'''
+Below code, is to demonstrate the utility text functions in python.
+'''
+# printing first character in "this is a text"
+character = text[0]
+print(f"First character in {text}: {character}")
+
+character = text[1]
+print(f"Second character in {text}: {character}")
+# in a text of length 5, what is the index of last character? 4
+# in a text of length 10, what is the index of last character? 9
+
+length = len(text)
+
+print(f"The length of {text} : {length}")
+
+last_character = text[len(text) -1]
+
+print(f"The last character of '{text}' is {last_character}")
+
+
+# python specific.
+last_character = text[-1]
+print(f"The last character of '{text}' is {last_character}")
+
+
+for line in open("python_code\\learning\\basics\\file.txt"):
+    print(line)
+
+
