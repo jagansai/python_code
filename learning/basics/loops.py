52253b2924f18ffc6e211faa222241fb378a1d99
Sai Jagannath <jagan.sai@outlook.com>
2024-04-07 11:31:21 +0800

    Added new code

---
+++ learning/basics/loops.py
@@ -0,0 +1,29 @@
+'''
+A code is not just about processing the statements one after the other.
+Sometimes, we need to loop through the same statements until the condition is met.
+Loops are
+ - for
+ - while
+'''
+
+''' 
+while loop: for a start, check the condition is met. step.
+ Step1: start = 1
+ Step2: check if start < 10
+ do work
+ increment start by 1
+ go to step#2
+'''
+
+'''
+print "hi" 10 times by using a while loop
+'''
+start = 1
+while start <= 10:
+    print(f"{start}: Hi")
+    start = start + 1
+
+'''
+print only even numbers less than 10 starting from 1.
+'''
+
