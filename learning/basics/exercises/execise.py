d09bfb9649319e39cd3a67a7b74e9e82d8a67a8b
jagansai <jagan.sai@outlook.com>
2024-10-03 17:36:18 +0800

    New exercise

---
+++ learning/basics/exercises/execise.py
@@ -0,0 +1,13 @@
+'''
+From an input, take a list of numbers... 
+
+have an array of numbers [1, 3, 4, 3, 5, 6 ]
+
+now get an input from prompt of a number.
+
+to_search = int(input("Enter a number: "))
+
+Now, in the array you need to check how many times the input number occurs.
+For e.g., in [1, 3, 4, 3, 5, 6 ] we have 3 occuring 2 times.
+Your output should be : 3 appeared 2 times in the array.
+'''
